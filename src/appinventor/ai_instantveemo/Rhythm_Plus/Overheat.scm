6e3fd525509c599c6a6bfe38f13242e0c178ef71
Splamei <129879626+splamei@users.noreply.github.com>
2024-06-30 11:55:27 +0100

    Add files via upload

---
+++ src/appinventor/ai_instantveemo/Rhythm_Plus/Overheat.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"228","Source":"Form","Properties":{"$Name":"Overheat","$Type":"Form","$Version":"31","AccentColor":"&HFFFFFFFF","ActionBar":"True","AlignHorizontal":"3","AlignVertical":"2","AppName":"Rhythm_Plus","BackgroundColor":"&HFF000000","PrimaryColor":"&HFF000000","PrimaryColorDark":"&HFF000000","ShowStatusBar":"False","Theme":"AppTheme","Title":"Overheat","TitleVisible":"False","Uuid":"0","$Components":[{"$Name":"Label1","$Type":"Label","$Version":"5","FontSize":"25","Width":"-1070","Text":"Your device is getting too hot. It is not safe to continue as you device might shutdown without warning due to this. Wait a few minutes to a few hours for your device to cool down and play again","TextAlignment":"1","TextColor":"&HFFFFFFFF","Uuid":"-219638630"}]}}
+|#
